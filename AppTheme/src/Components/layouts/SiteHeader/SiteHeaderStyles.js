bab53a92a7db1963c4a0b620a98696bc3ca03c9c
Kier Musngi <musngi.kier@gmail.com>
2018-08-25 23:24:22 +0800

    Added pre-mature site header

---
+++ AppTheme/src/Components/layouts/SiteHeader/SiteHeaderStyles.js
@@ -0,0 +1,15 @@
+const HeaderStyles = {
+    root: {
+        flexGrow: 1,
+    },
+    flex: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+        color: "inherit",
+    }
+};
+
+export default HeaderStyles;
