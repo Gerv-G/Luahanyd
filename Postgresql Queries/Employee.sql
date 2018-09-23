fc86081275d0d1b6ad334d3c2738c8190692f6fb
Kier Musngi <musngi.kier@gmail.com>
2018-09-23 14:56:59 +0800

    Introducing Npgsql interface with Dockerized PostgreSQL Database

---
+++ Postgresql Queries/Employee.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Employees
+(
+    EmployeeNumber varchar(10),
+    FirstName varchar(30),
+    MiddleName varchar(30),
+    LastName varchar(30),
+    Gender bit,
+    BirthDate timestamp,
+    MobileNumber varchar(10),
+    TelephoneNumber varchar(20),
+    EmailAddress varchar(50),
+    HomeAddress varchar(150),
+    SssNumber varchar(20),
+    BirNumber varchar(20),
+    TaxIdentificationNumber varchar(20),
+    PhilHealthNumber varchar(20),
+    HdmfNumber varchar(20)
+);
---
+++ TMS/PostgresqlDBInterface/Program.cs
@@ -0,0 +1,36 @@
+﻿using Npgsql;
+using System;
+
+namespace PostgresqlDBInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Save?");
+            var ans = Console.ReadLine();
+
+            if(ans == "y")
+            {
+                Save();
+                Console.WriteLine("Saved");
+            }
+        }
+
+        private static void Save()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=root;" +
+                                "Password=Asdf1234;Database=TMS;");
+            conn.Open();
+
+            // Define a query
+            NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO payroll.name VALUES ('Engr. Kier Musngi, CpE');", conn);
+
+            // Execute a query
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            // Close connection
+            conn.Close();
+        }
+    }
+}
